8864ca9688b15125ec6b1b3256f8fa4c24438e30
Vipin Yadav <yadavipin57@gmail.com>
2024-06-28 11:53:50 +0530

    Deleted folders

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import {Outlet, outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
-function Layout(){
-    return (
-        <>
-        
-        <Header/>
-        <Outlet/>
-        <Footer/>
-        
-        </>
-    )
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
 }
 
 export default Layout
